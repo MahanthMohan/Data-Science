cbdd8d561fe560241355232217afa482a87e41a5
Mahanth Mohan <mohan.mahanth@gmail.com>
2020-07-08 22:09:43 -0700

    fixed Algorithms

--- Algorithms/Classification-Metrics.py
+++ Algorithms/Classification-Metrics.py
@@ -1,27 +1,30 @@
 import numpy as np
 
 def Jaccard(y, y_hat):
-    Union = lambda y, y_hat: [element for element in y and y_hat]
-    Jaccard = np.abs(len(Union(y, y_hat)))/(np.abs(len(y) + len(y_hat)) - np.abs(Union(len(y), len(y_hat))))
+    Union = []
+    for i in range(len(y)):
+        if y[i] == y_hat[i]:
+            Union.append(y[i])
+    Jaccard = np.abs(len(Union))/(np.abs(len(y) + len(y_hat)) - np.abs(len(Union)))
     return Jaccard
 
 def ConfusionMatrix(Confusion_Matrix, value):
     precision = lambda TP, FP: TP/(TP + FP)
     recall = lambda TP, FN: TP/(TP + FN)
     if value == 1:
-        TP = ConfusionMatrix[0][0]
-        TN = ConfusionMatrix[1][1]
-        FP = ConfusionMatrix[1][0]
-        FN = ConfusionMatrix[0][1]
+        TP = Confusion_Matrix[0][0]
+        TN = Confusion_Matrix[1][1]
+        FP = Confusion_Matrix[1][0]
+        FN = Confusion_Matrix[0][1]
         precision = precision(TP, FP)
         recall = recall(TP, FN)
         return [f'precision: {precision}', f'recall: {recall}']
 
     elif value == 2:
-        TP = ConfusionMatrix[1][1]
-        TN = ConfusionMatrix[0][0]
-        FP = ConfusionMatrix[0][1]
-        FN = ConfusionMatrix[1][0]
+        TP = Confusion_Matrix[1][1]
+        TN = Confusion_Matrix[0][0]
+        FP = Confusion_Matrix[0][1]
+        FN = Confusion_Matrix[1][0]
         precision = precision(TP, FP)
         recall = recall(TP, FN)
         return [f'precision: {precision}',f'recall: {recall}']
@@ -31,13 +34,13 @@ def F1_score(precision, recall):
     return F1_score
 
 def log_loss(y, y_hat):
-    n = len(y)
-    log_loss = 0
-    for i in range(n):
-        equation = (y * y_hat) + (1 - y) * np.log(1 - y_hat)
-        log_loss = log_loss + equation
+    log_loss = (y * y_hat) + (1 - y) * np.log(1 - y_hat)
     return log_loss
 
-y = [0, 1, 1, 0, 1, 1, 1, 1]
-y_hat = [0, 1, 1, 0, 1, 1, 1, 1]
+y = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+y_hat = [1, 1, 0, 0, 0, 1, 1, 1, 1, 1]
+values = "6, 9, 1, 24".split(",")
+confusion_matrix = np.array([values]).reshape(2,2)
 print(Jaccard(y, y_hat))
+print(ConfusionMatrix(confusion_matrix, 1))
+print(ConfusionMatrix(confusion_matrix, 1))
