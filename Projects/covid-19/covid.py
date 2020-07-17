57daa9671f691b61a611220f151129dbbf042285
root <root@ubuntu-20.04>
2020-07-16 17:05:14 -0700

    finished covid-19 model

--- Projects/covid-19/covid.py
+++ Projects/covid-19/covid.py
@@ -11,20 +11,30 @@ df.head()
 df['dateRep'] = pd.to_datetime(df['dateRep']).dt.strftime("%Y%m%d")
 
 
-condition = pd.notnull(df[['dateRep', 'cases', 'deaths', 'Cumulative_number_for_14_days_of_COVID-19_cases_per_100000']])
+condition = pd.notnull(df[['dateRep', 'cases', 'Cumulative_number_for_14_days_of_COVID-19_cases_per_100000']])
 cdf = df[condition]
 cdf.head()
 
 x_data, y_data = (cdf['dateRep'].values, cdf['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000'].values)
 
-coef = curve_fit(function, x_data, y_data)[0]
+xdata, ydata = ([],[])
+for value in x_data:
+    res = float(value)/float(max(x_data))
+    xdata.append(res)
+
+for value in y_data:
+    ret = float(value)/float(max(y_data))
+    ydata.append(ret)
+
+coef = curve_fit(function, xdata, ydata)[0]
 print('The coefficients are: ')
 print(coef)
 
 fig = plt.figure()
-plt.plot(x_data, y_data, 'bo')
 x = np.linspace(0, len(x_data), 1)
-plt.plot(x, function(x, coef[0], coef[1], coef[2], coef[3]), 'r')
+plt.plot(x_data, y_data, 'ro', label='data')
+plt.plot(x, function(x, *coef), linewidth=4.0, label='model')
+plt.legend(loc='best')
 plt.xlabel('Time (in days)')
 plt.ylabel('COVID cases per 100000')
 plt.show()
