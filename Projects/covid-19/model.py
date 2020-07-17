bc98c719b655664c1a6d3a6a7b23b5b7fe970581
Mahanth Mohan <mohan.mahanth@gmail.com>
2020-07-17 13:29:33 -0700

    added some things

--- Projects/covid-19/model.py
+++ Projects/covid-19/model.py
@@ -3,10 +3,7 @@ import pandas as pd
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-def function(x,a,b,c,d):
-    return a*(x**3) + b*(x**2) + c*x + d
-
-df = pd.read_csv('Data/Dataset/dataset.csv')
+df = pd.read_csv('Data/Dataset/covid.csv')
 df.head()
 df['dateRep'] = pd.to_datetime(df['dateRep']).dt.strftime("%Y%m%d")
 
@@ -14,25 +11,27 @@ condition = pd.notnull(df[['dateRep', 'cases', 'Cumulative_number_for_14_days_of
 cdf = df[condition]
 cdf.head()
 
+# Initialize the x and y values
+x, y = (cdf['dateRep'].values, cdf['cases'].values)
 x_data, y_data = (cdf['dateRep'].values, cdf['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000'].values)
+leng = len(x)
 
-# Coverting random string values in the data to float and normalizing it
-xdata, ydata = ([],[])
-for i in range(len(x_data)):
-    xdata.append(float(x_data[i])/float(max(x_data)))
-    ydata.append(float(x_data[i])/float(max(x_data)))
+# Main Figure Initialization
+fig, (ax1, ax2) = plt.subplots(2)
+fig.suptitle('COVID-19 models (Cubic relationships)')
 
-# Fitting a curve to the data using the curve_fit function from the sklearn.optimize package
-coef = curve_fit(function, xdata, ydata)[0]
-print('The coefficients are: ')
-print(coef)
+# Figure 1 (Total Number of Covid cases)
+ax1.plot(x, y, 'bo')
+ax1.set_xticks(np.arange(len(x)), minor=True)
+ax1.set_xticklabels(' ')
+ax1.set_xlabel('Time (in days)')
+ax1.set_ylabel(f'COVID-19 cases in USA')
 
-# Plotting the model
-leng = len(x_data)
-x = np.linspace(-120, 120, 120)
-fig = plt.figure()
-plt.plot(x, function(x, *coef), linewidth=3.0, label='curve model')
-plt.legend(loc='best')
-plt.xlabel('Time (in days)')
-plt.ylabel('COVID cases per 100000')
+# Figure 2 (Covid cases per 100000)
+ax2.plot(x_data, y_data, 'ro')
+plt.xticks(np.arange(len(x_data)), ' ')
+ax2.set_xlabel(f'Time (in days for {leng} days)')
+ax2.set_ylabel('COVID cases per 100000')
+
+# Display the plotted figure with the sub plots
 plt.show()
